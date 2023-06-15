cd9f695ee650055ae52c68963970663101bca9c8
sachiachar <136439833+sachiachar@users.noreply.github.com>
2023-06-15 11:49:59 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "linode_api_token" {
+    sensitive = true
+}
+
+variable "k8s_label" {
+  default       = "bookstore-operations"
+}
+
+variable "k8s_version" {
+  default       = "1.25"
+}
+
+variable "k8s_region" {
+  default       = "ap-southeast"
+}
+
+variable "k8s_tags" {
+  type = list(string)
+  default       = ["terraform-k8s"]
+}
+
+
+variable "k8s_node_count" {
+  type = number
+  default = 1
+}
+
+variable "k8s_node_type" {
+  default       = "g6-standard-1"
+}
