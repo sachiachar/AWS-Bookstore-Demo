f5721d8c24a8d6ddad88b30e47fcd8c10d0f7c8f
sachiachar <136439833+sachiachar@users.noreply.github.com>
2023-06-15 12:01:30 +0800

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -1,1 +1,18 @@
+resource "linode_lke_cluster" "bookstore-operations" {
+    label       = var.k8s_label
+    k8s_version = var.k8s_version
+    region      = var.k8s_region
+    tags        = var.k8s_tags
 
+    pool {
+        type  = var.k8s_node_type
+        count = var.k8s_node_count
+    }
+}
+
+
+resource "local_file" "k8s_config" {
+    content = "${nonsensitive(base64decode(linode_lke_cluster.bookstore-operations.kubeconfig))}"
+    filename = "${local.k8s_config_file}"
+    file_permission = "0600"
+}
