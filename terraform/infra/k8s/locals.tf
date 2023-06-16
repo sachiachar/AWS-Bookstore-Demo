47f1f54169d33a3d8d78d8b461bdfb3453be403c
Sachidananda Karkala <skarkala@akamai.com>
2023-06-16 14:22:38 +0800

    Adding tf outputs

--- terraform/infra/k8s/locals.tf
+++ terraform/infra/k8s/locals.tf
@@ -3,3 +3,7 @@ locals {
     k8s_config_dir = "${local.root_dir}/.kube/"
     k8s_config_file = "${local.root_dir}/.kube/kubeconfig.yaml"
 }
+
+output "k8s_config_file" {
+  value = k8s_config_file
+}
