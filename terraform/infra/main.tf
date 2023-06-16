c13c626db66f2e1f42a283ca2e92d1a249254370
sachiachar <136439833+sachiachar@users.noreply.github.com>
2023-06-16 11:53:38 +0800

    Update main.tf

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -19,12 +19,11 @@ provider "linode" {
   token = var.linode_api_token
 }
 
-#provider "helm" {
-#  kubernetes {
-#    config_path = "~/.kube/config"
-#    
-#  }
-#}
+provider "helm" {
+  kubernetes {
+    config_path = local.k8s_config_file    
+  }
+}
 
 
 
