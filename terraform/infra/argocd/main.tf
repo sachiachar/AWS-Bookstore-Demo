bc2ae5c63976229dbb8a85e0c764b3cc637ca03d
Sachidananda Karkala <skarkala@akamai.com>
2023-06-30 15:07:54 +0800

    Adding kubeconfig details in kubernetes provider-fixing kubectl not found issue 3

--- terraform/infra/argocd/main.tf
+++ terraform/infra/argocd/main.tf
@@ -36,7 +36,7 @@ data "terraform_remote_state" "kubeconfig" {
 
 # Create a kubeconfig file locally for operations
 resource "local_file" "k8s_config_ops" {
-  content         = nonsensitive(data.terraform_remote_state.kubeconfig.outputs.k8s_config_value_ops)
+  content         = data.terraform_remote_state.kubeconfig.outputs.k8s_config_value_ops
   filename        = local.k8s_config_file_ops
   file_permission = "0600"
 }
